9055aa0c898a69fafed7299d2ad0416cba8c4650
Ryan Roat <ryanroat@gmail.com>
2018-01-15 17:33:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function theBeatlesPlay(musicians,instruments) {
   let members = musicians.length;
   
   for (let i = 0; i < members; i++) {
-    band[i] = `${musicians[i]} play ${instruments[i]}`;
+    band[i] = `${musicians[i]} plays ${instruments[i]}`;
   }
   return band;
 }
